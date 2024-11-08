28e1634c33df55d32c371bb4cb90724a544bc632
Mikael Ayala dos Santos <106713704+mikael-ayala@users.noreply.github.com>
2024-11-08 16:47:59 -0300

    Add files via upload

--- arduino_clock.ino
+++ arduino_clock.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Ultrasonic.h>
+#include <Keypad.h>
 
 //Inicializa o display no endereco 0x27
 LiquidCrystal_I2C lcd(0x27,16,2);
@@ -11,10 +12,32 @@ Ultrasonic ultrasonic(4, 5);
 RTC_DS3231 rtc;
 
 int distance, horaDefinida, minutoDefinido, segundoDefinido;
+int contadorLed = 0;
+String hora, minuto;
 
 bool alarmeLigado = false;
+bool setarAlarme = false;
 
 const int BUZZER = 2;
+
+unsigned long startMillis;
+unsigned long currentMillis = 0;
+const unsigned long PERIOD = 1000;
+
+const byte ROWS = 4; 
+const byte COLS = 3;
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+
+byte rowPins[ROWS] = {13, 12, 11, 10}; 
+byte colPins[COLS] = {9, 8, 7};
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
  
 void setup()
 {
@@ -37,37 +60,76 @@ void setup()
   }
 
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  startMillis = millis();
 }
  
 void loop()
 {
-  noTone(BUZZER);
-  lcd.clear();
-  lcd.setBacklight(HIGH);
-  lcd.setCursor(0,0);
+  currentMillis = millis();
 
-  DateTime now = rtc.now();
+  char customKey = customKeypad.getKey();
+  
+  if (customKey) {
+    if (customKey == '#') {
+      Serial.print(customKey);
+      setarAlarme = true;
+    } else if (setarAlarme) {
+      Serial.print(customKey);
+      if (contadorLed < 5) {
+        lcd.setCursor(contadorLed++, 1);
+        lcd.print(customKey);
 
-  if (now.hour() < 10) lcd.print("0");
-  lcd.print(now.hour(), DEC);
-  lcd.print(" : ");
-  if (now.minute() < 10) lcd.print("0");
-  lcd.print(now.minute(), DEC);
-  lcd.print(" : ");
-  if (now.second() < 10) lcd.print("0");
-  lcd.print(now.second(), DEC);
+        if (contadorLed < 3) {
+          hora += customKey;
+        }
+        
+        if (contadorLed == 2) {
+          lcd.setCursor(contadorLed++, 1);
+          lcd.print(":");
+        }
 
-  distance = ultrasonic.read();
+        if (contadorLed > 3) {
+          minuto += customKey;
+        }
 
-  lcd.setCursor(0, 1);
-  Serial.print("distance " + distance);
-  lcd.print(rtc.getTemperature());
+        if (contadorLed == 5) {
+          horaDefinida = hora.toInt();
+          minutoDefinido = minuto.toInt();
 
-  if (now.hour() == horaDefinida && now.minute() == minutoDefinido && now.second() == segundoDefinido) {
-    ligarAlarme();
+          Serial.print("HORARIO DEFINIDO");
+          Serial.print(horaDefinida);
+          Serial.print(minutoDefinido);
+        }
+      }
+    }
   }
 
-  delay(1000);
+  if (currentMillis - startMillis >= PERIOD) { //test whether the period has elapsed
+    noTone(BUZZER);
+    //lcd.clear();
+    lcd.setBacklight(HIGH);
+    lcd.setCursor(0,0);
+
+    DateTime now = rtc.now();
+
+    if (now.hour() < 10) lcd.print("0");
+    lcd.print(now.hour(), DEC);
+    lcd.print(" : ");
+    if (now.minute() < 10) lcd.print("0");
+    lcd.print(now.minute(), DEC);
+    lcd.print(" : ");
+    if (now.second() < 10) lcd.print("0");
+    lcd.print(now.second(), DEC);
+
+    distance = ultrasonic.read();
+
+    if (now.hour() == horaDefinida && now.minute() == minutoDefinido && now.second() == segundoDefinido) {
+      ligarAlarme();
+    }
+
+    startMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
+  }
 }
 
 void ligarAlarme() {
